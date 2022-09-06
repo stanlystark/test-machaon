77852e6a26b7e86c8d21e8b5bc7eeeb51624433d
Stark <adm@justenjoy.ru>
2022-09-06 16:50:10 +0300

    fix key:gen

--- setup.sh
+++ setup.sh
@@ -1,12 +1,6 @@
 echo "Установка приложения..."
 docker-compose create -d --build
 sh ./start.sh
-echo "Установка Composer"
-docker exec -it -d test_machaon_app composer install
-echo "Копирование настроек окружения"
-docker exec -it -d test_machaon_app cp .env.example .env
-echo "Генерация ключа"
-docker exec -it -d test_machaon_app php artisan key:gen
-echo "Запуск миграции"
-docker exec -it -d test_machaon_app php artisan migrate
+echo "Настройка окружения"
+docker exec test_machaon_app bash -c "composer install ; cp .env.example .env ; php artisan key:generate ; php artisan migrate"
 echo "Установка завершена"
